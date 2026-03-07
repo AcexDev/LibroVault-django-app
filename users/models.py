8fca0b711ecb42ba714ae6c15648b4ec10fb969d
AcexDev <abdulrazaqm003@gmail.com>
2026-03-07 15:37:36 +0100

    Fixed app crash due to missing profile image

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from PIL import Image
 from library.models import Book
 from django.core.exceptions import ValidationError
-
+import os
 
 # Create your models here.
 class Profile(models.Model):
@@ -16,12 +16,13 @@ class Profile(models.Model):
     
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-        img = Image.open(self.image.path)
+        if self.image and os.path.exists(self.image.path):
+            img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(self.image.path)
 
 class UserBook(models.Model):
     STATUS_CHOICES = [
