65f0f0b419b79437e6c264144bfcbfc1d9a93c13
AcexDev <abdulrazaqm003@gmail.com>
2026-03-11 15:06:36 +0100

    Minor issue

--- bookcollection/settings.py
+++ bookcollection/settings.py
@@ -74,15 +74,9 @@ STORAGES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-if not DEBUG:
-
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -132,7 +126,8 @@ if not db_url:
 
 DATABASES = {
     'default': dj_database_url.parse(
-        db_url
+        db_url,
+        conn_max_age=600
     )
 }
 
